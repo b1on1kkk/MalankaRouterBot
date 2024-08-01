d1875c828dbe9f48e626c50bbb955e240dde2a40
b1on1kkk <b1on1kkk@mail.ru>
2024-08-01 15:46:05 +0300

    init ability to see bot available

--- src/main.py
+++ src/main.py
@@ -8,6 +8,10 @@ load_dotenv()
 from context import Connection
 from telebot.async_telebot import AsyncTeleBot
 
+# check if bot is up and running
+from keep_alive import keep_alive
+keep_alive()
+
 async def main():
     async with Connection() as conn:
             bot = AsyncTeleBot(os.getenv("BOT_TOKEN"), parse_mode="HTML")
